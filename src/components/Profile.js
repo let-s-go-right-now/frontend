27c97df73386eb31d10899615635b9e55920f177
nakyeong <yunna3425@naver.com>
2025-02-19 17:35:25 +0900

    Feat: 프로필 슬라이드 구현

---
+++ src/components/Profile.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import LeaderBadge from '../assets/images/leader_badge.png';
+import { Image, TouchableOpacity } from 'react-native';
+
+const ProfileWrapper = styled(TouchableOpacity)`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    opacity: ${({selected}) => (selected ? 1 : 0.2)};
+`
+
+const ProfileImage = styled(Image)`
+    width: 60px;
+    height: 60px;
+    border-radius: 30px;
+    border-width: ${({selected}) => (selected ? '2.5px' : '0px')};
+    border-color: ${({selected}) => (selected ? '#363638' : 'transparent')};
+`
+
+const Info = styled.View`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
+    gap: 4px;
+    margin-top: 8px;
+`
+
+const ColorBadge = styled.View`
+    width: 5px;
+    height: 5px;
+    border-radius: 30px;
+    background-color: ${({color}) => color};
+`
+
+const Name = styled.Text`
+    font-size: 15px;
+    font-family: 'SUIT-SemiBold';
+    color: #1D1D1F;
+`
+
+const Profile = ({
+    name,
+    leader,
+    sameName,
+    image,
+    color,
+    selected,
+    onPress
+}) => {
+    return (
+            <ProfileWrapper selected={selected} onPress={onPress}>
+                <ProfileImage 
+                    source={image} 
+                    selected={selected}
+                />
+                <Info>
+                    {sameName && <ColorBadge color={color}></ColorBadge>}
+                    <Name>{name}</Name>
+                    {leader && <Image source={LeaderBadge}/>}
+                </Info>
+            </ProfileWrapper>
+    )
+}
+
+export default Profile
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import BlackButton from "./BlackButton";
 import PlusButton from "./PlusButton";
 import { OptionButton, CategoryOptionButton } from "./OptionButton";
 import { OptionButton2, TransportOptionButton } from "./OptionButton2";
+import Profile from "./Profile";
+import ProfileSlide from "./ProfileSlide";
 
-export { BlackButton, PlusButton, OptionButton, CategoryOptionButton, OptionButton2, TransportOptionButton };
+export { BlackButton, PlusButton, OptionButton, CategoryOptionButton, OptionButton2, TransportOptionButton, Profile, ProfileSlide };
