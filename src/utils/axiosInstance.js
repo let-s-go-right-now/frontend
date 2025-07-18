f20719316c45bd62cc1fb3d66cee525296a18a5f
nakyeong <yunna3425@naver.com>
2025-07-18 22:56:28 +0900

    Feat: baseURL을 .env 파일에서 불러오도록 수정

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://letsgorightnow.store/',
+    baseURL: import.meta.env.BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
